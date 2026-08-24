26a3d943205203d1a9de7c213b887b0c31fc8487
Aisa Lotfi <aisalotfi2706@gmail.com>
2026-08-24 16:06:38 +0330

    Updates

--- _next/static/yLHcbhGFCmbD6LTA2Ssj2/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fcase-study\u002Fsadaf-stone","\u002F[locale]\u002Fresume"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/yatzs_mBmRNZupOkAuMfr/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fcase-study\u002F[slug]","\u002F[locale]\u002Fresume"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
